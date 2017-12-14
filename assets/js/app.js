64a9277fded8518da36a4dcefd9dc42e3c4ff01f
Bastet1982 <oriele.nieto@gmail.com>
2017-12-14 16:50:48 -0300

    seudo estructura lyft

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(){
+
+$(function(){           // función para activar el timer del efecto splash
+   setTimeout(function() {     
+      $('#splash').fadeOut(500);     // id splash del html, donde la funcion fadeOut desaparecerá a los 5 seg la pantalla de inicio
+   }, 2500);
+});
+
+
+});
+
